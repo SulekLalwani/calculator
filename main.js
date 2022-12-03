f4eab29bdd4c009a709d46906e1910231ae4e38b
Sulek Lalwani <73684484+SulekLalwani@users.noreply.github.com>
2022-12-03 10:01:14 +0530

    Merge refactor branch to master
    Changes made:
    
    1. Rewrite the if statements for the calculator logic, making them clearer and more maintainable than the ones that are used now, which are cluttered and have nested if statements.
    
    2. Remove the addition of commas to the numbers in the input string, and instead just add them directly to the numbers in the input area in the page.

--- main.js
+++ main.js
@@ -2,13 +2,31 @@ let input = "";
 
 function recordPress(button) {
   if (button.textContent == "CE") {
-    input = input
-      .slice(0, -1)
-      .replace(/,/g, "")
-      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, "$&,");
+    input = input.slice(0, -1);
   } else if (button.textContent == "AC") {
     input = "";
-    document.getElementsByTagName("button")[0].innerText = "CE";
+  } else if (button.textContent.match(/\d/)) {
+    if (!input.match(/%$/g)) {
+      input += button.textContent;
+    }
+  } else if (button.textContent.match(/[-+×÷]/)) {
+    if (input.match(/[\d%]$/g)) {
+      input += button.textContent;
+    }
+  } else if (button.textContent == ".") {
+    if (input.match(/(^|[-+×÷])\d*$/g)) {
+      input += button.textContent;
+    }
+  } else if (button.textContent == "%") {
+    if (input.match(/\d$/g)) {
+      input += button.textContent;
+    }
+  } else if (button.textContent == "/") {
+    if (!input.match(/([-+÷×]|^)-\d*\.?\d*$/g)) {
+      input = input.replace(/\d+\.?\d*$/g, "-$&");
+    } else if (input.match(/([-+÷×]|^)-\d*\.?\d*$/g)) {
+      input = input.replace(/-(?=\d*\.?\d*$)/g, "");
+    }
   } else if (button.textContent == "=") {
     input = eval(
       input
@@ -16,51 +34,28 @@ function recordPress(button) {
         .replace(/÷/g, "/")
         .replace(/--/g, "+")
         .replace(/%/g, "/100")
-        .replace(/,/g, "")
-    ).toLocaleString();
+    ).toString();
     document.getElementsByTagName("button")[0].innerText = "AC";
-  } else if (button.textContent == "/") {
-    if (!input.replace(/,/g, "").match(/([-+÷×]|^)-\d*\.?\d*$/g)) {
-      input = input
-        .replace(/,/g, "")
-        .replace(/\d+\.?\d*$/g, "-$&")
-        .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, "$&,");
-    } else if (input.replace(/,/g, "").match(/([-+÷×]|^)-\d*\.?\d*$/g)) {
-      input = input
-        .replace(/,/g, "")
-        .replace(/-(?=\d*\.?\d*$)/g, "")
-        .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, "$&,");
-    }
-  } else {
-    if (input.match(/[^-+÷×]$/g) || button.textContent.match(/\d/)) {
-      if (!input.match(/\.\d*$/g) || button.textContent.match(/[^.]/)) {
-        if (input.match(/[^%]$/g) || button.textContent.match(/[^%]/)) {
-          if (
-            !input.replace(/,/g, "").match(/%$/g) ||
-            !button.textContent.match(/[\d.]/)
-          ) {
-            input += button.textContent;
-            input = input
-              .replace(/,/g, "")
-              .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, "$&,");
-            if (document.getElementsByTagName("button")[0].innerText == "AC") {
-              document.getElementsByTagName("button")[0].innerText = "CE";
-            }
-          }
-        }
-      }
-    }
   }
-  document.getElementsByTagName("textarea")[0].value = input;
+
+  if (button.textContent != "=") {
+    document.getElementsByTagName("button")[0].innerText = "CE";
+  }
+
+  document.getElementsByTagName("textarea")[0].value = input.replace(
+    /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,
+    "$&,"
+  );
+
   document.getElementsByTagName("textarea")[0].scrollLeft =
     document.getElementsByTagName("textarea")[0].scrollWidth;
 }
 
 function updateInput() {
-  input = document
-    .getElementsByTagName("textarea")[0]
-    .value.replace(/,/g, "")
-    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, "$&,");
+  input = document.getElementsByTagName("textarea")[0].value.replace(/,/g, "");
 
-  document.getElementsByTagName("textarea")[0].value = input;
+  document.getElementsByTagName("textarea")[0].value = input.replace(
+    /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,
+    "$&,"
+  );
 }
